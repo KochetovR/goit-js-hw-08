4d40b0b47d772c8f6377d1e66ea948dbae11a1e1
Kochetov Roman <k04erg0@gmail.com>
2021-04-29 16:43:55 +0300

    complete HW-08

---
+++ js/gallery.js
@@ -0,0 +1,97 @@
+import galleryArr from './gallery-items.js';
+let i = -1;
+let k = 0;
+const createGalleryItem = ({preview, original, description}) => {
+    i+=1
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img 
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    index="${i}"/>
+    </a>
+    </li>`;
+};
+
+const makeGallery = galleryArr.map(par => createGalleryItem(par)).join('');
+const galleryList = document.querySelector('.js-gallery');
+galleryList.insertAdjacentHTML('beforeend',makeGallery);
+
+const popap = document.querySelector('.js-lightbox')
+const previewImg = document.querySelectorAll('.gallery__image')
+const originImgInModal = document.querySelector('.lightbox__image');
+const backdropDiv = document.querySelector('.lightbox__overlay')
+const closeBtn = document.querySelector('[data-action="close-lightbox"]')
+let newOriginImgInModal = ''
+
+galleryList.addEventListener('click', onOpenModal);
+
+function onOpenModal(e) {
+    k = Number(e.target.getAttribute('index'));
+
+    console.log(e.target.getAttribute('index'))
+    e.preventDefault();
+    window.addEventListener('keydown', onEscKeyPress)
+
+    if(e.target.nodeName !== 'IMG') { 
+        return
+    }
+    popap.classList.add('is-open');
+
+    originImgInModal.src=`${e.target.dataset.source}`;
+    originImgInModal.alt=`${e.target.alt}`; 
+}
+// смена изображения нажатием клавиш "влево" или "вправо"
+document.addEventListener('keydown', onLeftOrRightKeyPress)
+function onLeftOrRightKeyPress (ev) {
+if(ev.key === 'ArrowRight') {
+    newOriginImgInModal = previewImg[k+1]
+    originImgInModal.src=`${newOriginImgInModal.dataset.source}`;
+    originImgInModal.alt=`${newOriginImgInModal.alt}`;
+    k = Number(newOriginImgInModal.getAttribute("index"))
+}
+
+if(ev.key === 'ArrowLeft') {
+    newOriginImgInModal = previewImg[k-1]
+    originImgInModal.src=`${newOriginImgInModal.dataset.source}`;
+    originImgInModal.alt=`${newOriginImgInModal.alt}`;
+    k = Number(newOriginImgInModal.getAttribute("index"))
+}
+
+    // закртыие модалки по клику кнопке "закрыть"
+closeBtn.addEventListener('click', onCloseBtnClick);
+function onCloseBtnClick() {
+    closeModal()
+}
+
+// зыкрытие модалки по клику backdrop
+backdropDiv.addEventListener('click', onBackdropClick)
+function onBackdropClick(ev) {
+    if(ev.currentTarget === ev.target) {
+        closeModal()
+    }
+}
+}
+
+function closeModal() {
+    popap.classList.remove('is-open')
+    originImgInModal.src=''
+    originImgInModal.alt=''
+    window.removeEventListener('keydown', onEscKeyPress)
+    k = 0;
+    console.log(k)
+}
+
+// зыкрытие модалки по клавише Esc
+function onEscKeyPress (event) {
+    if(event.code === 'Escape') {
+        closeModal() 
+    }
+}
+
+
+
+   
